8821396264086a31dce274276308d3151e982edf
Guerder Vieira <g7.vieira@gmail.com>
2020-09-05 12:43:41 -0300

    Realizado melhorias em todo o projeto

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,13 @@ const routes = require('express').Router();
 const Movie = require('./models/Movie')
 
 routes.get('/movies', async (req, res) => {
-    const movies = await Movie.find()
+    const limit = 12;
+    const page = req.query.page || 1;
+    const skip = (page - 1) * limit;
 
-    const serializedMovies = movies.filter((user, index, array) => index < 100).map(movie => {
+    const movies = await Movie.find().skip(skip).limit(limit);
+
+    const serializedMovies = movies.map(movie => {
         return {
             id: movie._id,
             title: movie.title, 
--- frontend/src/components/Article/index.js
+++ frontend/src/components/Article/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect} from 'react';
-import { Container, Row, Col, Card, CardImg, CardTitle, CardText, CardBody, Button } from 'reactstrap';
+import { Row, Col, Card, CardImg, CardTitle, CardText, CardBody, Button } from 'reactstrap';
+
+import './styles.css'
 
 import api from '../../services/api';
 
@@ -8,35 +10,63 @@ import notFound from '../../assets/not-found.png';
 const Article = () => {
 
   const [movies, setMovies] = useState([]);
+
+  const [page, setPage] = useState(1);
   
   useEffect(() => {
     async function loadMovies() {
-      const MovieList = await api.get('/movies');
+      const MovieList = await api.get(`/movies?page=${page}`);
       setMovies(MovieList.data);
     }
 
     loadMovies();
-  }, []);
+  }, [page]);
+
+  function getMovies(event){
+    if(event.target.value === "prox"){
+      setPage(page + 1);
+    }else{
+      setPage(page - 1);
+    }
+  }
   
   return (
-    <Container fluid>
+    <>
+      <Row className="mt-5">
+        <Col md="6">
+          <Button 
+            className="btn-lg btn-block" 
+            color="dark"
+            value="ant"
+            onClick={getMovies}
+          >Anterior</Button>
+        </Col>
+        <Col md="6">
+          <Button 
+            className="btn-lg btn-block" 
+            color="dark"
+            value="prox"
+            onClick={getMovies}
+          >Próximo</Button>
+        </Col>
+      </Row>
       <Row>
       {movies &&
         movies.map(movie => (
-          <Col md="2" key={movie.id}>
-            <Card>
-              <Button>{movie.title}
+          <Col md="2" key={movie.id} className="mt-5">
+            <Card inverse className="text-white bg-dark">
+              <Button>
                 <CardImg src={notFound} alt={movie.title} />
               </Button>
               <CardBody>
                 <CardTitle>{movie.title}</CardTitle>
-                <CardText>{movie.overview}</CardText>
+                <CardText>{movie.overview.substring(0, 100).trim().concat("...")}</CardText>
               </CardBody>
             </Card>
           </Col>
         ))}
       </Row>
-    </Container>
+    </>
     );
   }
   
